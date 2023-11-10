f7ca6927081260b9f7ee2580203b828e0c77d947
Almer Toro Coskun <alco0001@stud.kea.dk>
2023-11-10 13:37:08 +0100

    y/n to isHuman works

--- src/userinterface/InputHelper.java
+++ src/userinterface/InputHelper.java
@@ -34,15 +34,28 @@ public class InputHelper {
             }
         }
     }
-
     public boolean promptBoolean(String prompt) {
         while (true) {
             try {
+                // Viser prompt besked til brugeren
                 System.out.println(prompt);
-                return Boolean.parseBoolean(keyboard.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter 'y' if human or 'n' if non-human.");
-                // Line break added for readability
+                // Læser brugerinput og konverterer det til små bogstaver for at undgå forskelle i store og små bogstaver
+                String input = keyboard.nextLine().toLowerCase();
+                // Tjekker om input er 'y' eller 'yes', returner true
+                if (input.equals("y") || input.equalsIgnoreCase("yes")) {
+                    return true;
+                    //Tjekker om input er 'n' eller 'no', returner false
+                } else if (input.equals("n") || input.equalsIgnoreCase("no")) {
+                    return false;
+                    // Viser en fejlmeddelelse for ugyldig input
+                } else {
+                    System.out.println("Invalid input. Please enter 'y'/'yes' if human or 'n'/'no' if non-human.");
+                    System.out.println();
+                }
+            }
+            // Viser en fejlmeddelelse for ugyldig input
+            catch (Exception e) {
+                System.out.println("Invalid input. Please try again.");
                 System.out.println();
             }
         }
